6bb4f3269e8c71f96f5df2a01991691953b41f69
Douglas Vaz <douglas1992@gmail.com>
2014-11-03 10:15:20 +0530

    Use Mocha for tests  - Update package.json  - Add gulp task 'test'  - Sample test under 'test'

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp')
   , nodemon = require('gulp-nodemon')
   , jshint = require('gulp-jshint')
+  , mocha = require('gulp-mocha');
 
 gulp.task('lint', function () {
   gulp.src('./**/*.js')
     .pipe(jshint())
 })
+
+gulp.task('test', function(){
+  return gulp.src(['test/*.js'], {read: false})
+	.pipe(mocha({reporter: 'spec'}));
+});
 
 gulp.task('serve', function () {
   nodemon({ script: 'server.js', ext: 'html js', ignore: ['ignored.js'] })
