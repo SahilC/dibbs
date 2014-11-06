bed6212822de68af4e9176bd8e293d582cec1b66
Douglas Vaz <douglas1992@gmail.com>
2014-11-06 15:44:48 +0530

    Read auth tokens from env variables  - 'gulp serve' sets up environment from config/env/env.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,12 @@ gulp.task('test', function(){
 });
 
 gulp.task('serve', function () {
+  //Load env
+  var env = require('./config/env/env');
+  for(var key in env) {
+    process.env[key] = env[key];
+  }
+
   nodemon({ script: 'server.js', ext: 'html js', ignore: ['ignored.js'] })
     .on('change', ['lint'])
     .on('restart', function () {
